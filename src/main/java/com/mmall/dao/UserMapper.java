04bf4e91188cec8edd14c1c2c6d38d5c819bbd3e
小键子 <chengjunjian@126.com>
2019-01-01 17:46:29 +0800

    update

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -11,7 +12,24 @@ public interface UserMapper {
 
     User selectByPrimaryKey(Integer id);
 
+    User selectLogin(@Param("username") String username,@Param("password")String password);
+
+    int checkUsername(String username);
+
+    int checkEmail(String email);
+
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    String selectQuestionByUsername(String question);
+
+    //获取答案是否正确
+    int checkAnswer(String username,String question,String answer);
+
+    int updatePasswordByUsername(@Param("username")String username,@Param("passwordNew") String passwordNew);
+
+    int checkPassword(@Param(value="password")String password,@Param(value="userId") Integer userId);
+
+    int checkEmailByUserId(@Param(value = "email")String email,@Param(value = "userId") Integer userId);
 }
